5ca530711a5baf966cb99ce8cda2db5696594c16
Richard <richard89@debian.bcrowngroup.com>
2021-04-18 23:16:30 -0400

    2222

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -7,26 +7,29 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Level;
-//import java.util.Scanner;
 import java.util.logging.Logger;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.configuration.RunConfiguration;
 import com.configuration.RunConfiguration.OSType;
 import com.console.RunConsole;
 import com.constants.StringConstants;
 import com.logger.CustomLogger;
-
+import com.util.DirectoryUtils;
 
 public class Main {	
 	
 
     protected static HashMap<Variables, String> variablesMap;
+    protected static HashMap<Variables, String> variablesNames;
     public static String[] errorList;
     private static File suitePath;
     public File getSuitePath() {return suitePath;}
-
+    
 	
 	protected static enum Variables{
 		PROJECT_VAR,
@@ -37,6 +40,7 @@ public class Main {
 		FAILURE_VAR
 	}
 	
+			
 	 private static final String HEAD = StringConstants.CYAN_TEXT+ "NRE>" + StringConstants.ANSI_RESET;
 	 private static final String QUIT_COMMAND = "quit";
 	 private static final String CLEAR_COMMAND = "clear";
@@ -47,12 +51,20 @@ public class Main {
 	 private static final String BROWSER = "browserType";
 	 private static final String REPORT = "reportFolder";
 	 private static final String FAILURE = "failureHandling";
+	 private static int maxLength = 0;
 	 
-	 //project_123 pcte.us.to
-	 //static final String COMMAND = "-projectPath=/home/richard89/git/project123/PCTE_Tests -testSuitePath=Test Suites/9001-9500 Smoke/S9001 All Smoke Tests -browserType=Chrome -executionProfile=Authentication -failureHandling=stop";
-     //static final String COMMAND = "-projectPath=/home/richard89/Documents/new/Sebastian-2-master2/Sebastian-2-master/PCTE-Similar1 -testSuitePath=Smoke -browserType=Chrome(headless) -failureHandling=continue";
-	 //static final String COMMAND = "-projectPath=/home/richard89/Downloads/scripts/PCTE Tests -testSuitePath=Test Suites/9001-9500 Smoke/S9001 All Smoke Tests -browserType=Chrome -executionProfile=RCS01 QA01";
-	 static final String COMMAND = "-projectPath=/home/richard89/git/project123/PCTE_Tests -testSuitePath=Test Suites/9001-9500 Smoke/C9001 All Smoke Tests Across All Browsers -failureHandling=continue";
+		static {
+			
+			variablesNames = new HashMap<>();
+			
+		 	variablesNames.put(Variables.PROJECT_VAR, PROJECT);
+		 	variablesNames.put(Variables.TEST_SUITE_VAR, TEST_SUITE);
+		 	variablesNames.put(Variables.PROFILE_VAR, PROFILE);
+		 	variablesNames.put(Variables.BROWSER_VAR, BROWSER);
+		 	variablesNames.put(Variables.REPORT_VAR, REPORT);
+		 	variablesNames.put(Variables.FAILURE_VAR, FAILURE);		
+			
+		}
 	 		
 	 private static final String LOGO = StringConstants.CYAN_TEXT + "\n"
 			 				  + "******************WELCOME TO******************\n"
@@ -65,7 +77,7 @@ public class Main {
      						  + "##     ##  ##   ##        ##    ##            \n"
      						  + "##      ## ##   ##         ##   ##            \n"
      						  + "##       ####   ##          ##  ############# \n"
-     						  + "********The new runtime engine for PCTE*******\n"
+     						  + "*****The new runtime engine for PCTE-v1.0*****\n"
      						  + StringConstants.ANSI_RESET;
 	 
 	 private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -76,7 +88,7 @@ public class Main {
 		 	variablesMap = new HashMap<>();
 		 	errorList = null;
 		    suitePath = null;
-		 
+	 
 		 	variablesMap.put(Variables.PROJECT_VAR, null);
 			variablesMap.put(Variables.TEST_SUITE_VAR, null);
 			variablesMap.put(Variables.PROFILE_VAR, null);
@@ -90,8 +102,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		long absStart = 0, absFinish = 0;
-		
-		System.out.println(LOGO);
 		
 		
 		RunConfiguration.setPlatform(System.getProperty("os.name"));
@@ -108,6 +118,7 @@ public class Main {
 		
 		if(args.length > 0) {
 			
+			initializeVariables();
 			
 			if(args.length == 1 && args[0].equalsIgnoreCase(HELP_COMMAND)) {
 				LOGGER.info("Displaying Help...");
@@ -115,57 +126,59 @@ public class Main {
 				LOGGER.info("Closing Program...");
 				System.exit(0);
 			}
-			
-			else if(validateInput(args) > 0)
-				;
+						
 			else {
-			
-				initializeVariables();
-				processInput(args);				
-			
-				if(validateVariables() > 0)
-					;
-				else {
+				
+				System.out.println(LOGO);
+				
+				if(validateInput(args)) {
+							
+					if(validateVariables() > 0)
+						;
+					else {
+						
+						printVariables();
 	
-					Thread executionThread = new Thread() {						
-						@Override
-						public void run(){
+						Thread executionThread = new Thread() {						
+							@Override
+							public void run(){
 							RunConsole.run();
-						}						
-					};
+							}						
+						};
 		
-					absStart = System.currentTimeMillis();
-					executionThread.start();
+						absStart = System.currentTimeMillis();
+						executionThread.start();
 			
-					try {
-						executionThread.join();
-					} catch (InterruptedException e) {
+						try {
+							executionThread.join();
+						} catch (InterruptedException e) {
 			
-						LOGGER.severe(e.getMessage());
-					}
+							LOGGER.severe(e.getMessage());
+						}	
 		
-					RunConfiguration.resetConfigurationData();
-					System.gc();
-					absFinish = System.currentTimeMillis();
-					System.out.println(StringConstants.ANSI_RESET);
-					System.out.print(StringConstants.WHITE_BACKGROUND + StringConstants.BLACK_BOLD_BRIGHT + "COMPLETED in ");
-					System.out.print(StringConstants.BLUE_BACKGROUND + StringConstants.WHITE_BOLD_BRIGHT + "(" 
+						RunConfiguration.resetConfigurationData();
+						System.gc();
+						absFinish = System.currentTimeMillis();
+						System.out.println(StringConstants.ANSI_RESET);
+						System.out.print(StringConstants.WHITE_BACKGROUND + StringConstants.BLACK_BOLD_BRIGHT + "COMPLETED in ");
+						System.out.print(StringConstants.BLUE_BACKGROUND + StringConstants.WHITE_BOLD_BRIGHT + "(" 
 								+ StringConstants.YELLOW_TEXT + getElapsedTime(Long.valueOf(absFinish - absStart)) 
 								+ StringConstants.WHITE_BOLD_BRIGHT + ")");
-					System.out.println(StringConstants.ANSI_RESET);
+						System.out.println(StringConstants.ANSI_RESET);
 				
-					LOGGER.info("COMPLETED in "+ getElapsedTime(Long.valueOf(absFinish - absStart)));
+						LOGGER.info("COMPLETED in "+ getElapsedTime(Long.valueOf(absFinish - absStart)));
 									
+					}
+					LOGGER.info("Closing Program...");					
+					System.exit(0);
 				}
-				LOGGER.info("Closing Program...");
-				System.exit(0);
 			}
 		}
 		else {
 		
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 			Boolean quit = Boolean.FALSE;
-			
+			System.out.println(LOGO);
 		
 			while(!quit) {
 						
@@ -188,47 +201,48 @@ public class Main {
 					}
 					else {
 					
-						if(line.compareTo("123") == 0)
+						/*if(line.compareTo("123") == 0)
 							processInput(COMMAND);				
 						//System.out.println(variables);
-				
-						else 
-							processInput(line);	
-				
-				
-						if(validateVariables() > 0)
-							;
-						else {
-					
-							Thread executionThread = new Thread() {						
-								@Override
-								public void run(){
-									RunConsole.run();
-								}						
-							};
+					else */
+						if(validateInput(line)) {
+									
+							if(validateVariables() > 0)
+								;
+							else {
+								
+								printVariables();
+								
+								Thread executionThread = new Thread() {						
+									@Override
+									public void run(){
+										RunConsole.run();
+									}						
+								};
 						
-							absStart = System.currentTimeMillis();
-							executionThread.start();
+								absStart = System.currentTimeMillis();
+								executionThread.start();
 							
-							try {
-								executionThread.join();
-							} catch (InterruptedException e) {
+								try {
+									executionThread.join();
+								} catch (InterruptedException e) {
 							
-								LOGGER.severe(e.getMessage());
-							}
+									LOGGER.severe(e.getMessage());
+								}
 						
-							RunConfiguration.resetConfigurationData();
-							System.gc();
-							absFinish = System.currentTimeMillis();
-							System.out.println(StringConstants.ANSI_RESET);
-							System.out.print(StringConstants.WHITE_BACKGROUND + StringConstants.BLACK_BOLD_BRIGHT + "COMPLETED in ");
-							System.out.print(StringConstants.BLUE_BACKGROUND + StringConstants.WHITE_BOLD_BRIGHT + "(" 
-									+ StringConstants.YELLOW_TEXT +getElapsedTime(Long.valueOf(absFinish - absStart)) 
-									+ StringConstants.WHITE_BOLD_BRIGHT + ")");
-							System.out.println(StringConstants.ANSI_RESET);
+								RunConfiguration.resetConfigurationData();
+								System.gc();
+								absFinish = System.currentTimeMillis();
+								System.out.println(StringConstants.ANSI_RESET);
+								System.out.print(StringConstants.WHITE_BACKGROUND + StringConstants.BLACK_BOLD_BRIGHT + "COMPLETED in ");
+								System.out.print(StringConstants.BLUE_BACKGROUND + StringConstants.WHITE_BOLD_BRIGHT + "(" 
+										+ StringConstants.YELLOW_TEXT +getElapsedTime(Long.valueOf(absFinish - absStart)) 
+										+ StringConstants.WHITE_BOLD_BRIGHT + ")");
+								System.out.println(StringConstants.ANSI_RESET);
 							
-							LOGGER.info("COMPLETED in "+ getElapsedTime(Long.valueOf(absFinish - absStart)));
-						}	
+								LOGGER.info("COMPLETED in "+ getElapsedTime(Long.valueOf(absFinish - absStart)));
+								}	
+						}
 					}
 				}
 
@@ -238,27 +252,182 @@ public class Main {
 		}
 	}
 	
-	protected static void processInput(String line) {
+	
+	
+	private static void printVariables() {
+						
+		addHeader(maxLength);
 		
+		
+        variablesMap.forEach((k,v) -> System.out.println(formatRow("|" + StringUtils.leftPad(variablesNames.get(k), 18) 
+        				+ " | " + StringUtils.rightPad(v, maxLength + 2) + "|")));
+        addFooter(maxLength);	
+        
+	}
+	
+	
+		
+		
+	
+	
+	private static void addHeader(int length) {
+		
+		String header = "", line = "";
+		line += "a-------------------b";
+		for(int i = 0; i <= length + 2; i++)
+			line += "-";
+		line += "c\n";
+        header += formatDiv(line);
+        
+        line = "|     PARAMETER     | ";
+        line += StringUtils.rightPad("VALUE",length + 2);
+        line += "|\n";        
+        header += formatRow(line);
+        
+        line = "d-------------------e";
+		for(int i = 0; i <= length + 2; i++)
+			line += "-";
+		line += "f\n";
+        header += formatDiv(line);
+        System.out.print(header);
+		
+	}
+	
+	private static void addFooter(int length) {
+		
+		String line = "g-------------------h";
+			for(int i = 0; i <= length + 2; i++)
+				line += "-";
+			line += "i\n";
+		System.out.println(formatDiv(line));
+		
+	}
+	
+	
+	 private static String formatRow(String str) {
+		 
+	        return str.replace("|", StringConstants.BLUE_TEXT + "\u2502" + StringConstants.ANSI_RESET);
+	    }
+
+	 private static String formatDiv(String str) {
+		 
+	        return  StringConstants.BLUE_TEXT + str.replace('a', '\u250c')
+	                								.replace('b', '\u252c')
+	                								.replace('c', '\u2510')
+	                								.replace('d', '\u251c')
+	                								.replace('e', '\u253c')
+	                								.replace('f', '\u2524')
+	                								.replace('g', '\u2514')
+	                								.replace('h', '\u2534')
+	                								.replace('i', '\u2518')
+	                								.replace('-', '\u2500')
+	               + StringConstants.ANSI_RESET;
+	    }
+	
+	protected static Boolean validateInput(String command) {		
+		
+		String line = null;
+		String[] commands = {};
+			
 		if(RunConfiguration.getPlatform().equals(OSType.WINDOWS)) {
-			line = line.replace(File.separator, StringConstants.ID_SEPARATOR); 
+			line = command.replace(File.separator, StringConstants.ID_SEPARATOR); 
 			line = line.replace("/", StringConstants.ID_SEPARATOR);  
     	
 		}
     
 		else if(RunConfiguration.getPlatform().equals(OSType.LINUX)){
-			line = line.replace(File.separator, StringConstants.ID_SEPARATOR); 
+			line = command.replace(File.separator, StringConstants.ID_SEPARATOR); 
 			line = line.replace("\\", StringConstants.ID_SEPARATOR);  
 		}	
 	
-			String[] arrOfStr = line.split(" -", 15);
+			commands = line.split(" ", 20);
 			
-			processInput(arrOfStr);
+			
+			if(commands.length == 0 || commands[0].isEmpty()) {
+				System.out.println("Nothing can be done with an empty command! Please try again!");
+				LOGGER.severe("Empty command entered.");
+				return Boolean.FALSE;			
+			}
+			
+			if(commands.length > 20) {
+				System.out.println(StringConstants.RED_BOLD + "You entered too many parameteres and unfortunately we have taken buffer overflow into account!\n"
+									+ "Please try again with the correct parameters!" + StringConstants.ANSI_RESET);
+				LOGGER.severe("Attempt to overflow the buffer. Input exceeded the valid number of parameters.");
+				return Boolean.FALSE;
+			}
+			
+				
+			return validateInput(commands);
 	}
+	
+	
+	protected static Boolean validateInput(String[] args) {		
+	
+		ArrayList<String> commands = new ArrayList<>();
+		int commandIndex = 0;
+			
+		if(args.length > 20) {
+			System.out.println(StringConstants.RED_BOLD + "You entered too many parameteres and unfortunately we have taken buffer overflow into account!\n"
+								+ "Please try again with a valid number of parameters!" + StringConstants.ANSI_RESET);
+			LOGGER.severe("Attempt to overflow the buffer. Input exceeded the valid number of parameters.");
+			return Boolean.FALSE;
+		}
+		
+		for(int i = 0; i < args.length; i++) {
+			
+			//System.out.println(args[i]);
+			
+			if(!args[i].startsWith("-", 0)) {
+				
+				if(commands.isEmpty()) {
+					System.out.println("'" + StringConstants.RED_BOLD + args[i] + StringConstants.ANSI_RESET
+							+ "' is not a valid parameter! Please use '" + StringConstants.BLUE_BOLD + "help" 
+							+ StringConstants.ANSI_RESET + "' command for assistance!");
+					LOGGER.severe("Invalid parameter entered : '" + args[i] + "'");
+					return Boolean.FALSE;
+				}
+				
+				commands.set(commandIndex - 1, commands.get(commandIndex - 1).concat(" ").concat(args[i]));
+				
+			}
+			else {
+				if(!isValidParameter(args[i])) {
+					
+					System.out.println("'" + StringConstants.RED_BOLD + args[i] + StringConstants.ANSI_RESET
+							+ "' is not a valid parameter! Please use '" + StringConstants.BLUE_BOLD + "help" 
+							+ StringConstants.ANSI_RESET + "' command for assistance!");
+					LOGGER.severe("Invalid parameter entered : '" + args[i] + "'");
+					return Boolean.FALSE;
+				}
+				else {					
+					commands.add(commandIndex++, args[i]);
+				}
+			}
+			
+		}
+
+		processInput(commands.toArray(new String[commands.size()]));		
+				
+		return Boolean.TRUE;
+	}
+	
+	protected static Boolean isValidParameter(String line) {
+		
+		if(line.contains(PROJECT))	return Boolean.TRUE;
+		else if(line.contains(TEST_SUITE)) return Boolean.TRUE;
+		else if(line.contains(PROFILE)) return Boolean.TRUE;
+		else if(line.contains(BROWSER)) return Boolean.TRUE;
+		else if(line.contains(REPORT)) return Boolean.TRUE;
+		else if(line.contains(FAILURE)) return Boolean.TRUE;
+		else return Boolean.FALSE;			
+			
+	}
+	
 	
 	protected static void processInput(String[] arrOfStr) {
 			
 		variablesMap.clear();
+				
 	
 		try{
 		        
@@ -369,12 +538,7 @@ public class Main {
 	    	
 	    } catch (Exception ex) {LOGGER.severe(ex.getMessage());}
 	}
-	
-	
-	protected static int validateInput(String[] args) {		
-		return 0;
-	}
-	
+		
 
 	protected static int validateVariables() {
 		
@@ -422,13 +586,28 @@ public class Main {
 			
 			//validate REPORT DIR---------------------------------------------------
 			if(variablesMap.get(Variables.REPORT_VAR) == null)
-				/*** This is the final directory ***/
-				//variablesMap.put(Variables.REPORT_VAR, RunConfiguration.getProjectDir() + StringConstants.ID_SEPARATOR 
-				//		+ StringConstants.DEFAULT_REPORT_DIR);
 				variablesMap.put(Variables.REPORT_VAR, StringConstants.DEFAULT_REPORT_DIR);
-		
-			new File(variablesMap.get(Variables.REPORT_VAR)).mkdirs();
+			else {			
+				if(variablesMap.get(Variables.REPORT_VAR).endsWith("/") || variablesMap.get(Variables.REPORT_VAR).endsWith("\\\\") || variablesMap.get(Variables.REPORT_VAR).endsWith("\\")) 
+					variablesMap.put(Variables.REPORT_VAR, variablesMap.get(Variables.REPORT_VAR).concat(StringConstants.DEFAULT_REPORT_DIR));
+				else {
+					System.out.println(variablesMap.get(Variables.REPORT_VAR));
+					variablesMap.put(Variables.REPORT_VAR, variablesMap.get(Variables.REPORT_VAR).concat(StringConstants.ID_SEPARATOR)
+							.concat(StringConstants.DEFAULT_REPORT_DIR));
+				
+				}
+			}
 			
+						
+			try {
+				new File(variablesMap.get(Variables.REPORT_VAR) + StringConstants.ID_SEPARATOR + "img").mkdirs();
+				DirectoryUtils.copyDirectory(StringConstants.LOGO_FOLDER, variablesMap.get(Variables.REPORT_VAR) + StringConstants.ID_SEPARATOR + "img");
+			}catch(Exception e) {
+				LOGGER.severe("Creation of report directory failed : " + e.getMessage());
+				System.out.println(StringConstants.RED_BOLD + "Report folder could not be created! " + StringConstants.ANSI_RESET 
+						+ e.getLocalizedMessage());
+				flag++;
+			}
 				RunConfiguration.setReportDir(variablesMap.get(Variables.REPORT_VAR));
 			//-------------------------------------------------------------------------
 			
@@ -460,7 +639,9 @@ public class Main {
 		}
 		//-------------------------------------------------------------------------
 
-			return flag;	
+		variablesMap.forEach((k,v) -> maxLength = (v.length() > maxLength) ? v.length() : maxLength);
+		
+		return flag;	
 	}
 	
 	private static String getElapsedTime(long elapsed) {
